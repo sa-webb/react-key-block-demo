ee01d9deb4fcaf71ba6cb24a60647a78d11bcd98
sa-webb <sawebb30@gmail.com>
2020-04-29 10:23:45 -0500

    example

--- src/App.js
+++ src/App.js
@@ -1,26 +1,42 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Components from "./components.js";
+
+import "./styles.css";
+
+const data = {
+  content: {
+    body: [
+      {
+        _uid: "BUY6Drn9e1",
+        component: "foo",
+        headline: "Foo"
+      },
+      {
+        _uid: "gJZoSLkfZV",
+        component: "bar",
+        title: "Bar"
+      },
+      {
+        _uid: "X1JAfdsZxy",
+        component: "foo",
+        headline: "Another headline"
+      },
+      {
+        _uid: "X1JAfdsZxy",
+        component: "foo",
+        headline: "Another head"
+      }
+    ]
+  }
+};
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Hello React</h1>
+      {data.content.body.map(block => Components(block))}
     </div>
   );
 }
 
-export default App;
+export default App
